bbe9d8bcda4263b552224ed16c5ca29717c440ab
man20820 <fatkhurrahmandjogja@gmail.com>
2023-05-17 11:59:04 +0700

    trying 10

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const address = '0.0.0.0'; // or '0.0.0.0' to listen on all available network interfaces
 
 app.get('/', (req, res) => {
-  res.send('Hello World!1');
+  res.send('Hello World!2');
 });
 
 app.listen(port, address, () => {
